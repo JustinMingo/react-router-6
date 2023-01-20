9703d89eebbc476931364c0c4fcd33e0b1b186af
JustinMingo <justmingo@gmail.com>
2023-01-20 13:45:33 +0100

    made buttons and a link

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export interface IAboutPageProps {}
 
 const AboutPage: React.FunctionComponent<IAboutPageProps> = (props) => {
+  const navigate = useNavigate();
   return (
     <div>
       <p>This is the about page.</p>
+      <button onClick={() => navigate(-1)}>Go Back</button>
     </div>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export interface IHomePageProps {}
 
 const HomePage: React.FunctionComponent<IHomePageProps> = (props) => {
+  const navigate = useNavigate();
   return (
     <div>
       <p>This is the home page.</p>
+      <Link to="/about">Go to the About page!</Link>
+      <br />
+      <button onClick={() => navigate("/about")}>Go to About</button>
     </div>
   );
 };
